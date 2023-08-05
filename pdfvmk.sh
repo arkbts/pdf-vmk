7242a5ca82a4d2f463614eed5f24cd6f04198102
Alexander + <a.krause@mac.com>
2023-08-06 01:45:36 +0200

    Update pdfvmk.sh

--- pdfvmk.sh
+++ pdfvmk.sh
@@ -9,4 +9,4 @@ tmpfile=$(mktemp)
 echo "~color{0 0 0.5} $2 ~color{0.5 0 0} $eingang1 ~color{0 0 0} $3 --$benutzer" | \
 enscript -q -e~ --margins=::-1: -B -f Arial-Bold10 -o- | ps2pdf - "$tmpfile"
     
-qpdf $1 --overlay "$tmpfile" --repeat=1-z -- $datei"_"$2$eingang2".pdf"
+qpdf --warning-exit-0 --no-warn $1 --overlay "$tmpfile" --repeat=1-z -- $datei"_"$2$eingang2".pdf"
